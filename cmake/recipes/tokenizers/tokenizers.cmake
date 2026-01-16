61ee157161fc722767deb3af40d24ec3c9d5f77f
Thomas Otero <totero@google.com>
2026-01-15 21:06:02 -0800

    Remove unused code

--- cmake/recipes/tokenizers/tokenizers.cmake
+++ cmake/recipes/tokenizers/tokenizers.cmake
@@ -43,7 +43,6 @@ if(NOT EXISTS "${TOKENIZER_LIB_CHECK}")
         -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
         -DCMAKE_POSITION_INDEPENDENT_CODE=ON
         "-DCMAKE_PREFIX_PATH=${ABSL_INSTALL_PREFIX};${PROTO_INSTALL_PREFIX};${SENTENCE_INSTALL_PREFIX}"
-        # "-DCMAKE_CXX_STANDARD_LIBRARIES=-lprotobuf -lutf8_range -Wl,--start-group -labsl_status -labsl_statusor -labsl_raw_logging_internal -labsl_base -labsl_throw_delegate -labsl_int128 -labsl_log_internal_check_op -labsl_log_internal_message -labsl_log_internal_nullguard -labsl_strings -labsl_string_view -labsl_synchronization -labsl_debugging_internal -labsl_time -labsl_time_zone -labsl_utf8_for_code_point -Wl,--end-group -lpthread"
   )
 
 else()
@@ -67,29 +66,3 @@ target_link_libraries(tokenizers_lib INTERFACE
   sentencepiece_libs 
   absl_libs
 )
-
-
-    # ExternalProject_Add(
-    #     tokenizers_external
-    #     GIT_REPOSITORY
-    #         https://github.com/mlc-ai/tokenizers-cpp
-    #     GIT_TAG
-    #         main
-    #     PREFIX
-    #         ${EXTERNAL_PROJECT_BINARY_DIR}/tokenizers-cpp
-    #     # PATCH_COMMAND ${CMAKE_COMMAND} -E echo "Performing surgery on tokenizers-cpp..."
-    #     #     COMMAND sed -i "s/add_subdirectory(sentencepiece/# add_subdirectory(sentencepiece/" <SOURCE_DIR>/CMakeLists.txt
-    #     #     COMMAND sed -i "s/target_link_libraries(tokenizers_cpp PRIVATE tokenizers_c sentencepiece-static/target_link_libraries(tokenizers_cpp PRIVATE tokenizers_c/" <SOURCE_DIR>/CMakeLists.txt
-    #     # CMAKE_ARGS
-    #     #     -DCMAKE_INSTALL_PREFIX=${SENTENCE_INSTALL_PREFIX}
-    #     #     -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
-    #     #     -DCMAKE_POLICY_DEFAULT_CMP0169=OLD
-    #     #     -DCMAKE_CXX_STANDARD=17
-    #     #     -DCMAKE_CXX_FLAGS=${CMAKE_CXX_FLAGS}
-    #     #     -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
-    #     #     -DCMAKE_POSITION_INDEPENDENT_CODE=ON
-    #     #     -DSPM_ENABLE_TCMALLOC=OFF
-    #     #     -DSPM_PROTOBUF_PROVIDER=package
-    #     #     -DSPM_ABSL_PROVIDER=package
-    #     #     -DCMAKE_EXE_LINKER_FLAGS=${THE_LINKER_HAMMER_STRING}
-    #     #     -DCMAKE_CXX_STANDARD_LIBRARIES=${THE_LINKER_HAMMER_STRING}
